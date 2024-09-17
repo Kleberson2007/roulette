3c651047cef605639955b1dd6a4d39f19e2ed6d4
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-09-17 15:55:03 -0300

    q

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div id="page">
+      <div id="titulo">
+
+      </div>
+      <div id="resultado">
+
+      </div>
+      <div id="possibilidades">
+        
+      </div>
+    </div>
+  );
+}
